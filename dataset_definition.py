eb79120ea57b524094173dd47c15fc246f765506
Jeronee Jennycloss <jjeronee@gmail.com>
2024-04-10 16:34:14 +0100

    Add the dataset definition

--- dataset_definition.py
+++ dataset_definition.py
@@ -11,7 +11,7 @@ index_date = "2023-10-01"
 
 dataset = create_dataset()
 
-dataset.configure_dummy_data(population_size=10)
+dataset.configure_dummy_data(population_size=100)
 
 # codelists
 
@@ -75,9 +75,23 @@ dataset.num_asthma_inhaler_medications = (
 
 # outcome variable
 
-dataset.date_first_admitted = (
+dataset.date_patient_was_first_admitted = (
     apcs.where(apcs.admission_date.is_after(index_date))
     .sort_by(apcs.admission_date)
     .first_for_patient()
     .admission_date
 )
+
+from ehrql import create_dataset
+from ehrql.tables.core import patients, medications
+dataset = create_dataset()
+dataset.define_population(patients.date_of_birth.is_on_or_before("1999-12-31"))
+asthma_codes = ["39113311000001107", "39113611000001102"]
+latest_asthma_med = (
+    medications.where(medications.dmd_code.is_in(asthma_codes))
+    .sort_by(medications.date)
+    .last_for_patient()
+)
+dataset.asthma_med_date = latest_asthma_med.date
+dataset.asthma_med_code = latest_asthma_med.dmd_code
+
